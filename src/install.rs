4e576bae7cf48db26a189c37339531bed6daee6c
Marin Minnerly <dminnerly@roblox.com>
2025-10-13 14:48:02 -0700

    Create a function for the new behavior

--- src/install.rs
+++ src/install.rs
@@ -1,8 +1,8 @@
 use log::{debug, info};
 use serde::Deserialize;
 use std::env::current_dir;
-use std::fs;
 use std::path::{Path, PathBuf};
+use std::{fs, vec};
 
 use crate::roblox::{
     fetch_roblox_deploy_history, fetch_roblox_packages, get_roblox_version_by_git_hash,
@@ -15,6 +15,63 @@ const INDEX_PATH: &str = "Packages/_Index";
 #[derive(Deserialize, Debug)]
 struct RotrieverPackageLockfile {
     dependencies: Option<Vec<String>>,
+}
+
+fn prune_unused_dependencies(dependencies: &Vec<String>, packages_path: &Path) {
+    info!("pruning unused dependencies");
+
+    info!("root dependencies to keep: {:?}", dependencies);
+
+    fn process_dependency(
+        dependency: &str,
+        packages_path: &Path,
+        dependencies_to_keep: &mut Vec<String>,
+    ) {
+        info!("keeping dependency: {}", dependency);
+
+        // TODO: Handle if there are multiple versions of a package. i.e. if we
+        // want to include Foundation as a dependency, we need to make sure we
+        // use the right one. Maybe the most up-to-date?
+
+        let dependency_path = packages_path.join(INDEX_PATH).join(dependency);
+
+        let lockfile_path = dependency_path.join(LOCKFILE_NAME);
+        let lockfile_content = fs::read_to_string(lockfile_path).expect("Failed to read lockfile");
+
+        let lockfile: RotrieverPackageLockfile =
+            toml::from_str(&lockfile_content).expect("Failed to parse TOML");
+
+        // TODO: Traverse over the dependencies to keep, use their lockfiles
+        // to determine which _other_ dependencies to keep, and then remove
+        // everything in the index and root of RobloxPackages that isn't in
+        // the keep list.
+
+        if let Some(deps) = lockfile.dependencies {
+            for sub_dependency in &deps {
+                let sub_dependency_parts: Vec<&str> = sub_dependency.split(" ").collect();
+                let sub_dependency_name = sub_dependency_parts[1];
+
+                info!("keeping dependency: {}", sub_dependency_name);
+
+                dependencies_to_keep.push(sub_dependency_name.to_string());
+
+                process_dependency(sub_dependency_name, packages_path, dependencies_to_keep);
+
+                // TODO: Go up into the index and process the sub-dependency
+
+                // TODO: Traverse over the dependencies of these
+                // dependencies and so on until we have the full picture
+            }
+        }
+    }
+
+    let mut dependencies_to_keep: Vec<String> = vec![];
+
+    for dependency in dependencies {
+        process_dependency(dependency, packages_path, &mut dependencies_to_keep);
+    }
+
+    info!("dependencies to keep: {:?}", dependencies_to_keep);
 }
 
 pub async fn install_roblox_packages(
@@ -69,31 +126,7 @@ pub async fn install_roblox_packages(
     }
 
     if let Some(dependencies) = dependencies {
-        debug!("dependencies to keep: {:?}", dependencies);
-        info!("pruning unused dependencies");
-
-        for dependency in dependencies {
-            // TODO: Handle if there are multiple versions of a package. i.e. if we
-            // want to include Foundation as a dependency, we need to make sure we
-            // use the right one. Maybe the most up-to-date?
-            let dependency_path = dest_path.join(INDEX_PATH).join(dependency);
-
-            let lockfile_path = dependency_path.join(LOCKFILE_NAME);
-            let lockfile_content =
-                fs::read_to_string(lockfile_path).expect("Failed to read lockfile");
-
-            let lockfile: RotrieverPackageLockfile =
-                toml::from_str(&lockfile_content).expect("Failed to parse TOML");
-
-            if let Some(deps) = lockfile.dependencies {
-                info!("keeping dependencies: {:?}", deps);
-            }
-
-            // TODO: Traverse over the dependencies to keep, use their lockfiles
-            // to determine which _other_ dependencies to keep, and then remove
-            // everything in the index and root of RobloxPackages that isn't in
-            // the keep list.
-        }
+        prune_unused_dependencies(dependencies, &dest_path);
     }
 
     info!(
